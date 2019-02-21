4f69c6a2157295c14a180f816a397026c9588fd3
Sawutjan <laqin3@gmail.com>
2019-02-20 21:12:06 -0500

    adding minor changes

---
+++ src/test/java/com/in28minutes/junit/helper/PracticeClass.java
@@ -0,0 +1,16 @@
+package com.in28minutes.junit.helper;
+
+import org.junit.Test;
+
+
+
+
+//this is for a git example
+public class PracticeClass {
+
+	@Test
+	public void test1() {
+		System.out.println("hi git hub!");
+		System.out.println("hello eclipse!");
+	}
+}
